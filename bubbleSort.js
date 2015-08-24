da49322c6f0a018c76873708adbe6f45efaedbf3
Lance <Lance@thelpstory.com>
2015-08-24 12:01:10 -0600

    exercise complete

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,5 +1,17 @@
 function bubbleSort(arr){
-
+  var newArr = [];
+  while (arr.length) {
+    var largest = Math.max();
+    var position = 0;
+    for (var i = 0; i < arr.length; i++) {
+      if(arr[i] > largest){
+        largest = arr[i];
+        position = i;
+      }
+    }
+    newArr.push(arr.splice(position,1)[0]);
+  }
+  return newArr.reverse();
 }
 
 module.exports = bubbleSort;
